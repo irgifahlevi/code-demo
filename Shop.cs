4c124dd88857eeb16f64b19006721f262e211943
anlsdwiUI <analistiana.dewi@ui.ac.id>
2023-04-05 10:09:22 +0700

    add shop

---
+++ Shop.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Shop
+{
+	public int Id { get; set; }
+	public string Name { get; set; }
+	public string Description { get; set; }
+	public int Price { get; set; }
+	
+}
